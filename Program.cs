e9b206a314079702e6a2795db2e908e9f282c000
alejandraroel <59518000+alejandraroel@users.noreply.github.com>
2020-02-10 19:02:09 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //this is where you're gonna write all the main functions of your code
+
+            Console.WriteLine("Welcome to the Movie List Application!");
+
+            Console.WriteLine("What category are you interested in?");
+
+            string userInput = Console.ReadLine();
+
+            for each userInput print List<movie>
+
+
+
+        }
+    }
+}
